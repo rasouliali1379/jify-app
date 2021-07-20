ec1ad026b74c84e7fa4ec6b7c59dd5a12e219bc9
Ali Rasouli <rasouliali1379@gmail.com>
2021-07-20 19:50:32 +0430

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/jify_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jify_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(JiffyApp());
+}
+
+class JiffyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
