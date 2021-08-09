0e9a4c39ae1a2b443369b6b25ebccaf8da42a8e0
Ali Rasouli <rasouliali1379@gmail.com>
2021-08-09 23:56:59 +0430

    Contact Us Page Implemented

--- lib/controllers/help_center_page_controller.dart
+++ lib/controllers/help_center_page_controller.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+import 'package:jify_app/controllers/global_controller.dart';
 import 'package:jify_app/modals/contact_us_modal.dart';
 import 'package:jify_app/repositories/app_repository.dart';
 import 'package:jify_app/utilities/utilities.dart';
 
 class HelpCenterPageController extends GetxController {
+  final _globalController = Get.find<GlobalController>();
   final _appRepository = AppRepository();
   final fullNameTextController = TextEditingController();
   final emailTextController = TextEditingController();
@@ -12,7 +14,42 @@ class HelpCenterPageController extends GetxController {
   final orderNumberTextController = TextEditingController();
   final messagesTextController = TextEditingController();
 
+  final _fullNameError = "".obs;
+  final _emailError = "".obs;
+  final _subjectError = "".obs;
+  final _messagesError = "".obs;
+
   final _loadingStatus = false.obs;
+
+  @override
+  void onInit() {
+    fillDefaultData();
+    super.onInit();
+  }
+
+  String get fullNameError => _fullNameError.value;
+
+  set fullNameError(String value) {
+    _fullNameError.value = value;
+  }
+
+  String get emailError => _emailError.value;
+
+  set emailError(String value) {
+    _emailError.value = value;
+  }
+
+  String get subjectError => _subjectError.value;
+
+  set subjectError(String value) {
+    _subjectError.value = value;
+  }
+
+  String get messagesError => _messagesError.value;
+
+  set messagesError(String value) {
+    _messagesError.value = value;
+  }
 
   bool get loadingStatus => _loadingStatus.value;
 
@@ -32,6 +69,12 @@ class HelpCenterPageController extends GetxController {
     subjectTextController.clear();
     orderNumberTextController.clear();
     messagesTextController.clear();
+    fullNameError = "";
+    emailError = "";
+    subjectError = "";
+    messagesError = "";
+
+    fillDefaultData();
   }
 
   void closeModal() {
@@ -39,16 +82,18 @@ class HelpCenterPageController extends GetxController {
   }
 
   void submitMessage() {
-    loadingStatus = true;
-    _appRepository
-        .contactSupport(
-            fullNameTextController.text,
-            emailTextController.text,
-            subjectTextController.text,
-            orderNumberTextController.text,
-            messagesTextController.text)
-        .then((value) =>
-            value.fold((l) => attemptFailed(l), (r) => attemptSucceed(r)));
+    if (validateFields()) {
+      loadingStatus = true;
+      _appRepository
+          .contactSupport(
+              fullNameTextController.text,
+              emailTextController.text,
+              subjectTextController.text,
+              orderNumberTextController.text,
+              messagesTextController.text)
+          .then((value) =>
+              value.fold((l) => attemptFailed(l), (r) => attemptSucceed(r)));
+    }
   }
 
   void attemptFailed(String message) {
@@ -60,5 +105,46 @@ class HelpCenterPageController extends GetxController {
     loadingStatus = false;
     Utilities.makeCustomToast(message);
     Get.back();
+    clearTextFields();
+  }
+
+  void fillDefaultData() {
+    if (_globalController.initialDataModel.user != null) {
+      final userData = _globalController.initialDataModel.user!;
+
+      if (userData.firstname != null && userData.lastname != null) {
+        fullNameTextController.text =
+            '${userData.firstname!} ${userData.lastname!}';
+        emailTextController.text = userData.email!;
+      }
+    }
+  }
+
+  bool validateFields() {
+    if (fullNameTextController.text.isEmpty) {
+      fullNameError = "You need to enter your full name";
+      return false;
+    }
+    fullNameError = "";
+
+    if (emailTextController.text.isEmpty) {
+      emailError = "You need to enter your email";
+      return false;
+    }
+    emailError = "";
+
+    if (subjectTextController.text.isEmpty) {
+      subjectError = "You need to enter a subject";
+      return false;
+    }
+    subjectError = "";
+
+    if (messagesTextController.text.isEmpty) {
+      messagesError = "Enter your message please";
+      return false;
+    }
+    messagesError = "";
+
+    return true;
   }
 }
--- lib/modals/contact_us_modal.dart
+++ lib/modals/contact_us_modal.dart
@@ -3,7 +3,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:jify_app/constants/app_colors.dart';
 import 'package:jify_app/constants/app_text_styles.dart';
-import 'package:jify_app/controllers/checkout_fragment_controller.dart';
 import 'package:jify_app/controllers/help_center_page_controller.dart';
 import 'package:jify_app/widgets/circle_button.dart';
 import 'package:jify_app/widgets/long_button.dart';
@@ -18,113 +17,152 @@ class ContactUsModal extends GetView<HelpCenterPageController> {
       child: Container(
         width: double.maxFinite,
         height: Get.height * 0.8349,
-        padding: EdgeInsets.all(Get.width * 0.0453),
         decoration: const BoxDecoration(
             color: AppColors.white,
             borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(15), topRight: Radius.circular(15))),
-        child: NoScrollGlow(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      'Contact Us',
-                      style: AppTextStyles.black16Normal600,
-                    ),
-                    CircleButton(
-                      Icon(
-                        Icons.clear,
-                        color: AppColors.darkGrey,
-                        size: Get.width * 0.06,
-                      ),
-                      AppColors.grey,
-                      controller.closeModal,
-                      height: Get.width * 0.0746,
-                      width: Get.width * 0.0746,
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: Get.height * 0.0147,
-                ),
-                const Text(
-                  'Delivery option and delivery speeds may very '
-                  'for different locations',
-                  style: AppTextStyles.grey14Normal400,
-                ),
-                SizedBox(
-                  height: Get.height * 0.0332,
-                ),
-                TitledTextField(
-                  'Fullname',
-                  controller.fullNameTextController,
-                  inputAction: TextInputAction.next,
-                  textFieldBgColor: AppColors.red.withOpacity(0.05),
-                ),
-                SizedBox(
-                  height: Get.height * 0.0197,
-                ),
-                TitledTextField(
-                  'Email',
-                  controller.emailTextController,
-                  inputAction: TextInputAction.next,
-                  textFieldBgColor: AppColors.red.withOpacity(0.05),
-                ),
-                SizedBox(
-                  height: Get.height * 0.0197,
-                ),
-                TitledTextField(
-                  'Subject',
-                  controller.subjectTextController,
-                  inputAction: TextInputAction.next,
-                  textFieldBgColor: AppColors.red.withOpacity(0.05),
-                ),
-                SizedBox(
-                  height: Get.height * 0.0197,
-                ),
-                TitledTextField(
-                  'Order Number',
-                  controller.orderNumberTextController,
-                  inputAction: TextInputAction.next,
-                  textFieldBgColor: AppColors.red.withOpacity(0.05),
-                ),
-                SizedBox(
-                  height: Get.height * 0.0197,
-                ),
-                TitledTextField(
-                  'Messages',
-                  controller.messagesTextController,
-                  inputAction: TextInputAction.next,
-                  customTextField: MultilineTextField(
-                    controller.messagesTextController,
-                    bgColor: AppColors.red.withOpacity(0.05),
-                    height: Get.height * 0.1256,
-                    hint: 'Type your message ...',
-                  ),
-                ),
-                SizedBox(
-                  height: Get.height * 0.0197,
-                ),
-                Obx(
-                  () => LongButton(
-                    controller.submitMessage,
-                    'Submit',
-                    Get.width,
-                    Get.height * 0.064,
-                    customText: controller.loadingStatus
-                        ? const SpinKitThreeBounce(
-                            color: AppColors.white,
-                            size: 15,
-                          )
-                        : null,
-                  ),
-                )
-              ],
+        child: Column(
+          children: [
+            SizedBox(
+              height: Get.height * 0.0209,
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: Get.width * 0.0453),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Contact Us',
+                    style: AppTextStyles.black16Normal600,
+                  ),
+                  CircleButton(
+                    Icon(
+                      Icons.clear,
+                      color: AppColors.darkGrey,
+                      size: Get.width * 0.06,
+                    ),
+                    AppColors.grey,
+                    controller.closeModal,
+                    height: Get.width * 0.0746,
+                    width: Get.width * 0.0746,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: Get.height * 0.0209,
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: Get.width * 0.0453),
+                child: NoScrollGlow(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: [
+                      SizedBox(
+                        height: Get.height * 0.0147,
+                      ),
+                      const Text(
+                        'Delivery option and delivery speeds may very '
+                        'for different locations',
+                        style: AppTextStyles.grey14Normal400,
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.0332,
+                      ),
+                      Obx(
+                        () => TitledTextField(
+                          'Full name',
+                          controller.fullNameTextController,
+                          inputAction: TextInputAction.next,
+                          textFieldBgColor: AppColors.red.withOpacity(0.05),
+                          errorText: controller.fullNameError.isNotEmpty
+                              ? controller.fullNameError
+                              : null,
+                        ),
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.0197,
+                      ),
+                      Obx(
+                        () => TitledTextField(
+                          'Email',
+                          controller.emailTextController,
+                          inputAction: TextInputAction.next,
+                          textFieldBgColor: AppColors.red.withOpacity(0.05),
+                          errorText: controller.emailError.isNotEmpty
+                              ? controller.emailError
+                              : null,
+                        ),
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.0197,
+                      ),
+                      Obx(
+                        () => TitledTextField(
+                          'Subject',
+                          controller.subjectTextController,
+                          inputAction: TextInputAction.next,
+                          textFieldBgColor: AppColors.red.withOpacity(0.05),
+                          errorText: controller.subjectError.isNotEmpty
+                              ? controller.subjectError
+                              : null,
+                        ),
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.0197,
+                      ),
+                      TitledTextField(
+                        'Order Number',
+                        controller.orderNumberTextController,
+                        inputAction: TextInputAction.next,
+                        textFieldBgColor: AppColors.red.withOpacity(0.05),
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.0197,
+                      ),
+                      Obx(
+                        () => TitledTextField(
+                          'Messages',
+                          controller.messagesTextController,
+                          inputAction: TextInputAction.next,
+                          errorText: controller.messagesError.isNotEmpty
+                              ? controller.messagesError
+                              : null,
+                          customTextField: MultilineTextField(
+                            controller.messagesTextController,
+                            bgColor: AppColors.red.withOpacity(0.05),
+                            height: Get.height * 0.1256,
+                            hint: 'Type your message ...',
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: Get.height * 0.0197,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Obx(
+              () => Padding(
+                padding: EdgeInsets.all(Get.width * 0.0453),
+                child: LongButton(
+                  controller.submitMessage,
+                  'Submit',
+                  Get.width,
+                  Get.height * 0.064,
+                  customText: controller.loadingStatus
+                      ? const SpinKitThreeBounce(
+                          color: AppColors.white,
+                          size: 15,
+                        )
+                      : null,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
